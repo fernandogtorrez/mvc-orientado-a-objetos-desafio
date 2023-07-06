d846912d0b25b24cde7dee81a76e4e9d51d994e8
fernandogtorrez <fernandogtorrez@gmail.com>
2023-07-05 19:53:39 -0500

    primer commit

--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,27 @@
+import * as jsonfile from 'jsonfile';
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  data: Contact[] = []
+  load(){
+    const json = jsonfile.readFileSync(__dirname + '/contacts.json')
+    this.data = json
+  }
+  getAll(){
+    return this.data
+  }
+  addOne(contact:Contact){
+    return this.data.push(contact)
+  }
+  save(){
+    return jsonfile.writeFileSync(__dirname + '/contacts.json',this.data)
+  }
+  getOneById(id){
+    return this.data.find((item)=> item.id == id)
+  }
+}
+export { ContactsCollection, Contact };
